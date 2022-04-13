2876e4202046d68d59c608c86d420d74d0db246c
krishnendu basak <krisauz17@yahoo.com>
2022-04-13 21:16:03 +1000

    this lab is done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+card = ["Ada", "Brendan", "Ali"];
+function writeCards(card, birthday) {
+    for (let i = 0; i < card.length; i++) {
+        console.log(`Thank you, ${card[i]} go the wonderful ${birthday} gift!`);
+    }
+        return card;
+}
+
+
+function countDown(num){
+    let countDown=10;
+    while(countDown<10){
+        console.log(num);
+        countDown--;
+    }
+    console.log(num);
+}
